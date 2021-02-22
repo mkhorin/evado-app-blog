655ff2550960d8ad414c77a98185cf347638f839
mkhorin <maksimovichu@gmail.com>
2021-02-23 08:53:25 +1000

    Add language toggle

--- config/default.js
+++ config/default.js
@@ -86,6 +86,7 @@ module.exports = {
     params: {        
         'enablePasswordReset': false,
         'enableSignUp': false,
-        'enableSignUpVerification': false
+        'enableSignUpVerification': false,
+        'languageToggle': true
     }
 };
